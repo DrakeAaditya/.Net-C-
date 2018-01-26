a7280ef19881a5050ab3498be1c637a803a408d4
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-01-26 17:14:40 +0530

    Create Take Input as String and display no. of times a sub-string occurs.cs
    works only for one letter

---
+++ Take Input as String and display no. of times a sub-string occurs.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Addy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int c = 0;
+            Console.WriteLine("Enter the String > ");
+            String str = Console.ReadLine();
+            int a = str.Length;
+            Console.WriteLine("Enter the Substring > ");
+            String sub = Console.ReadLine();
+            int b = sub.Length;
+
+            for (int i = 0; i < a; i++)
+            {
+                int j = 0;
+                if (str[i] == sub[j])
+                {
+                    count++;
+
+                    j++;
+                }
+                if (count == b)
+                {
+
+                    c++;
+                    count = 0;
+                }
+
+            }
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
