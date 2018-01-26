d4afa0dace97cb8be78cd7fe775a96eae47270d5
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-01-26 17:08:01 +0530

    Create To Convert to Integer.cs

---
+++ To Convert to Integer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+    }
+}
